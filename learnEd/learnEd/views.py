ac51ea1a8f4c0c724cfab8812f3bc9d28dce4a23
Anshuman Raj Chauhan <its7arc@gmail.com>
2021-04-10 18:29:39 +0530

    removing files

--- learnEd/learnEd/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,HttpResponseRedirect,Http404
-from django.urls import reverse
-from django.utils import timezone
